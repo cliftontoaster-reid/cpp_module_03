820a365c1e3ba16573921d31b88d511ed42d219f
Clifton Toaster Reid <clifton.toaster.reid@proton.me>
2025-09-01 17:05:21 +0200

    Add ClapTrap and ScavTrap classes with attack, damage, and repair functionalities; create main battle simulation

---
+++ ex01/ClapTrap.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <string>
+
+class ClapTrap {
+protected:
+  std::string name;
+  int hitPoints;
+  int energyPoints;
+  int attackDamage;
+
+public:
+  ClapTrap(const std::string &name);
+  ~ClapTrap();
+
+  void attack(const std::string &target);
+  void takeDamage(unsigned int amount);
+  void beRepaired(unsigned int amount);
+  void setAttackDamage(int damage);
+};
